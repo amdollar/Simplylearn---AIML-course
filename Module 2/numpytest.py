7e71cc25a21f3306cba716ff08bdc3ef9052c69d
Anurag <anuragawasthi020@gmail.com>
2025-08-23 15:31:30 +0530

    Making changes in module 2 NumPy

--- Module 2/numpytest.py
+++ Module 2/numpytest.py
@@ -4,15 +4,21 @@ ls = [1,2,3,4,5,6,7]
 
 npls = np.array(ls)
 
-print(npls.ndim)
-print(npls.max())
+# Get the dimentions of the array: ndim
+# print(npls.ndim)
 
+# Get the max element of the array:
+# print(npls.max())
+
+#Creation of a 2D array
 twoDArray = np.array([
     [1,2,3],
     [4,5,6]
 ])
 
-print(twoDArray)
-print(twoDArray.ndim)
+# print(twoDArray)
+# print(twoDArray.ndim)
 
-print(twoDArray[0:1,0:3])
+# Print attributes of an 2D array start and end range
+# start:end
+# print(twoDArray[0:1,0:3])
