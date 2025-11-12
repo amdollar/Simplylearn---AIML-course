07a9a12220ca1e4bec5ea29c97f8c1b0b18926b3
Anurag <anuragawasthi020@gmail.com>
2025-11-13 01:31:55 +0530

    Added new example for KNN Regressor for 50 start up example.

---
+++ Module 3/KNN50StartupImplementation.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings('ignore')
+
+data = pd.read_csv('50_Startups.csv')
+
+print(data.info())
+print(data.head())
+
+features = data.iloc[:, [0,1,2,3]].values
+label = data.iloc[:, [4]].values
+
+stateImputer = OneHotEncoder(sparse=False)
+stateImputer.fit(features[:, [3]])
+state_values = stateImputer.fit_transform(features[:, [3]])
+
+final_features = np.concatenate((state_values, features[:, [0,1,2]]),axis = 1)
+print(final_features)
+SL = 0.05
+CL = 1-SL
+for rs in range (1, 10):
+    for k in range(1, 20):
+        X_train, X_test, y_train, y_test = train_test_split(final_features, label, test_size=0.2, random_state=rs)
+
+        model = KNeighborsRegressor(n_neighbors= k)
+
+        model.fit(X_train, y_train)
+        train_score= model.score(X_train, y_train)
+        test_score= model.score(X_test, y_test)
+
+        if test_score > train_score and test_score >= CL:
+            print(f'Test score: {test_score}, Train score: {train_score}, Random state: {rs}, K value : {k}')
