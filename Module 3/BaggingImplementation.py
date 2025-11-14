eb7a33c3176e2593b8c5f42362fec879483394d0
Anurag <anuragawasthi020@gmail.com>
2025-11-14 14:13:34 +0530

    Adding code for Sampling with and without replacement

--- Module 3/BaggingImplementation.py
+++ Module 3/BaggingImplementation.py
@@ -43,9 +43,6 @@ X_train,X_test,y_train,y_test = train_test_split(features, label, test_size=0.2,
 model = KNeighborsClassifier()
 
 # This is the part where we will be doing Bagging:
-# i. Decide the learners size:
-# learners = int(round(np.sqrt(len(X_train))))
-
 
 # Default: Shuffling
 
@@ -55,6 +52,50 @@ ensemble_model = BaggingClassifier(n_estimators=5,
 
 ensemble_model.fit(X_train, y_train)
 #Train score:
+print('Baging with Shuffling technique: ')
 print(ensemble_model.score(X_train, y_train)) # 0.909375
 #Test score:
 print(ensemble_model.score(X_test, y_test)) # 0.7625
+
+# Baging with Shuffling technique: 
+# 0.909375
+# 0.7625
+# 2.1 Sampling data: (With Replacement)
+
+# i. Decide the learners size:
+learners = int(round(np.sqrt(len(X_train))))
+model2 = KNeighborsClassifier()
+sample_ensamble = BaggingClassifier(n_estimators= 5, 
+                                    base_estimator= model2,
+                                    max_samples=learners, # How many numbers of records per sample
+                                    bootstrap=True, # Sampling replacementactivated
+                                    random_state=3) # For reproducing output
+
+sample_ensamble.fit(X_train, y_train)
+print('Bagging with Sampling with replacement : ')
+print(sample_ensamble.score(X_train, y_train))
+print(sample_ensamble.score(X_test, y_test))
+# Bagging with Sampling with replacement :
+# 0.759375
+# 0.675
+# 2.2 Sampling data (Without replacement)
+
+sample_size = int(round(np.sqrt(len(X_train))))
+
+model3 = KNeighborsClassifier()
+sample_ensamble_wr = BaggingClassifier(n_estimators=5, 
+                                       base_estimator= model3,
+                                       max_samples=sample_size,
+                                       bootstrap=False,
+                                       random_state=3)
+
+sample_ensamble_wr.fit(X_train, y_train)
+
+print('Bagging with Sampling without replacement.')
+
+print(sample_ensamble_wr.score(X_train, y_train))
+print(sample_ensamble_wr.score(X_test, y_test))
+
+# Bagging with Sampling without replacement.
+# 0.759375
+# 0.675
