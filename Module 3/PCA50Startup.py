690541affbb1c64a565144fff82cb0b331cdebb8
Anurag <anuragawasthi020@gmail.com>
2025-11-20 17:25:18 +0530

    Code for deployment is added with PCA

--- Module 3/PCA50Startup.py
+++ Module 3/PCA50Startup.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression
+import warnings
+warnings.filterwarnings('ignore')
+
 
 data = pd.read_csv('50_Startups.csv')
 
@@ -18,8 +23,17 @@ encoded_state = encoder.fit_transform(features[:,[3]])
 final_features = np.concatenate((encoded_state, features[:,[0,1,2]]), axis=1)
 
 print(final_features)
+
+# Scale features and apply PCA on the full feature set, then train the model
+scaler = StandardScaler()
+scaled_all = scaler.fit_transform(final_features)
+
+# Reduce to 1 principal component (as originally intended)
+pca = PCA(n_components=1)
+pca_all = pca.fit_transform(scaled_all)
+
 model = LinearRegression()
-X_train, X_test, y_train, y_test = train_test_split(final_features, labels, test_size=0.2, random_state=2)
+X_train, X_test, y_train, y_test = train_test_split(pca_all, labels, test_size=0.2, random_state=2)
 
 model.fit(X_train, y_train)
 
@@ -39,7 +53,16 @@ while state not in valid_state_values:
 print(state)    
 
 state_i = encoder.transform(np.array([[state]]))
+print(state_i)
 input_features = np.concatenate((state_i, np.array([[rdSpend, administration, marketingSpend]])), axis = 1)
-print(input_features)
-print(model.predict(input_features))
-# [[52202.28961586]]
+
+
+
+# predicted_value = model.predict(input_features)
+# print(f'Predicted profit after PCA: {predicted_value}')
+# Apply the same scaler and PCA transforms to the single input sample, then predict
+scaled_input = scaler.transform(input_features)
+pca_input = pca.transform(scaled_input)
+
+predicted_profit = model.predict(pca_input)
+print(f'Predicted profit after PCA: {predicted_profit}')
