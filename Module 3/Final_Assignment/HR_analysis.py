54e4b68799c19a94693a1995b5d700c3eba7443c
Anurag <anuragawasthi020@gmail.com>
2025-11-22 12:47:30 +0530

    Added clustering class imbalance, traing and evaluate techniques

--- Module 3/Final_Assignment/HR_analysis.py
+++ Module 3/Final_Assignment/HR_analysis.py
@@ -2,6 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
 
 class HRAnalyzer:
 
@@ -70,7 +77,7 @@ class HRAnalyzer:
         plt.title('Correlation Matrix - Numerical Features', fontsize= 14, fontweight = 'bold')
         plt.tight_layout()
         plt.savefig('Correlationheatmap.png', dpi = 300, bbox_inches= 'tight')
-        plt.show()
+        # plt.show()
 
         # 2.2 Distribution Plot:
         print('\n 2.2. Creation of distribution plots..')
@@ -96,7 +103,7 @@ class HRAnalyzer:
 
         plt.tight_layout()
         plt.savefig('distribution_plots.png', dpi= 300, bbox_inches = 'tight')
-        plt.show()
+        # plt.show()
 
         # 2.3. Bar plot of project count by employee status
         print('\n Creating bar plot for Project count analysis..')
@@ -108,7 +115,7 @@ class HRAnalyzer:
         plt.legend(title='Employee Status', labels=['Stayed', 'Left'])
         plt.tight_layout()
         plt.savefig('project_count_analysis.png', dpi = 300, bbox_inches= 'tight')
-        plt.show()
+        # plt.show()
 
         self._print_eda_insights()
 
@@ -133,7 +140,175 @@ class HRAnalyzer:
         print(f'Average satisfaction of emplyees who left: {avg_satisfaction_left:.2f}')
         print(f'Average satisfaction of emplyees who stayed: {avg_satisfaction_stayed:.2f}')
 
+    def employee_clustering(self):
+        'Plateform k-Mean clustering of employees who left'
+        print('\n' + '=' * 60)
+        print('Employee clustering analysis')
+        print('\n' + '=' * 60)
+
+        # 3.1 Select relevant cols to filter employees who left
+        print('\n 3.1 Selecting employees who left company..')
+        left_employees = self.df[self.df['left']==1][['satisfaction_level', 'last_evaluation', 'left']]
+        print(f'Employees who left: {left_employees}')
+        print(f'Number of Employees who left: {len(left_employees)}')
+
+        # 3.2 Perform K-Means clustering
+        print('\n 3.2 Perform K-means clustering into 3 clusters..')
+        clustering_data = left_employees[['satisfaction_level', 'last_evaluation']]
+
+        k_means = KMeans(n_clusters=3, random_state=42, n_init=10)
+        clusters = k_means.fit_predict(clustering_data)
+
+        # Add cluster labels to the data
+        left_employees_clustered = left_employees.copy()
+        left_employees_clustered['cluster'] = clusters
+
+        # Visualize clusters
+        plt.figure(figsize=(12,8))
+        scatter = plt.scatter(left_employees_clustered['satisfaction_level'], left_employees_clustered['last_evaluation'],
+                              c = clusters, cmap = 'viridis', alpha=0.6, s=50)
+        
+        plt.scatter(k_means.cluster_centers_[:,0], k_means.cluster_centers_[:,1], c='red', marker= 'x', s=200, linewidths=3, label='Centorids')
+
+        plt.xlabel('Satisfaction level', fontsize=12)
+        plt.ylabel('Last Evaluation', fontsize=12)
+        plt.title('K-mean clusterig of Employees who left \n (Based on Satisfaction & Evaluation)', fontsize=12, fontweight='bold')
+        plt.colorbar(scatter, label='Cluster')
+        plt.legend()
+        plt.grid(True, alpha=0.3)
+        plt.tight_layout()
+        plt.savefig('employee_clustering.png', dpi=300, bbox_inches='tight')
+        plt.show()
+
+        # 3.3 Analyze clsuter
+        self._analyze_clusters(left_employees_clustered, k_means.cluster_centers_)
+        return left_employees_clustered, k_means
+
+    def _analyze_clusters(self, clustered_data, centroids):
+        '''Analyze and interpret the clusters'''
+        print('\n 3.3 Cluster Analysis: ')
+        print('-' * 40)
+
+        for i in range(3):
+            cluster_data = clustered_data[clustered_data['cluster'] ==i]
+            avg_satisfaction = cluster_data['satisfaction_level'].mean()
+            avg_evaluation = cluster_data['last_evaluation'].mean()
+
+            count= len(cluster_data)
+
+            print(f'\n Cluster {i}:')
+            print(f'Count : {count } employees ({count/len(cluster_data) * 100 :.1f}%)')
+            print(f'Average Satisfaction: {avg_satisfaction:.2f}')
+            print(f'Average evaluation: {avg_evaluation:.2f}')
+
+            # Interpretation
+
+            if avg_satisfaction < 0.5 and avg_evaluation < 0.7:
+                interpretation = 'Low performers with low satisfaction'
+            elif avg_satisfaction <0.5 and avg_evaluation >=0.7:
+                interpretation = 'High performers but un-satisfied (burnout risk)'
+            else: 
+                interpretation = 'Moderately satisfied employees'
+
+            print(f'Interpretation: {interpretation}')   
+
+    def handle_class_imbalance(self):
+        '''Handle the class imbalance using SMOTE technique'''
+
+        print('\n' + '=' * 60)
+        print('4. Handling class Imbalance with SMOTE')
+        print('=' * 60)
+
+        # 4.1 Preprocess data
+        print('4.1 Preprocessing data')
+        
+        # Saperate the categorical and numerical variables
+        categorical_cols = self.df.select_dtypes(include=['object']).columns.tolist()
+        numerical_cols = self.df.select_dtypes(include=['np.number']).columns.tolist()
+
+        print(f'Categorical columns: {categorical_cols}')
+        print(f'Numerical columns: {numerical_cols}')
+
+        # Remove target variable from the numerical cols:
+        if 'left' in numerical_cols:
+            numerical_cols.remove('left')
+
+        # Apply get_dummies to categorical variables
+        df_encoded = pd.get_dummies(self.df[categorical_cols], drop_first=True)
+
+        # combined with numerical valriables
+        X = pd.concat([self.df[numerical_cols], df_encoded], axis=1)
+        y = self.df['left']
+
+        print(f'Final feature matrix shape: {X.shape}')
+        print(f'Target distribution: \n{y.value_counts()}')
+
+        # 4.2 Startified train-test split
+
+        print('\n 4.2 Performing stratified train-test split (80-20)...')
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size = 0.2, random_state= 123, startify= y)
+        
+        print(f'Train set shape: {self.X_train.shape}')
+        print(f'Test set shape: {self.X_test.shape}')
+        print(f'Train target distribution: \n {self.y_train.value_counts()}')
+
+        # 4.3
+        print('\n 4.3 Applying SMOTE to balance the training set...')
+        smote = SMOTE(random_state=123)
+        self.X_train_balanced, self.y_train_balanced = smote.fit_resample(self.X_train, self.y_train)
+
+        print(f'After SMOTE - Train set shape: {self.X_train_balanced.shape}')
+        print(f'After SMOTE - Target distribution: \n {pd.Series(self.y_train_balanced).value_counts()}')
+
+        return X, y
     
+    def train_and_evaluate_models(self):
+        '''Train models with 5-fold corss-validation'''
+        print('\n' + '=' *60)
+        print('5. Model training and evaluation')
+        print('=' * 60)
+
+        # Initialize models
+
+        models = {'Logistic Regression': LogisticRegression(random_state=123, max_iter=1000),
+                  'Random Forest': RandomForestClassifier(random_state=123, n_estimators=100),
+                   'Gradient Boosting': GradientBoostingClassifier(random_state=123, n_estimators=100) }
+        
+        # 5-fold cross-validation
+
+        from sklearn.model_selection import StratifiedKFold
+        cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=123)
+
+        for name, model in models.items():
+            print(f'\n 5.{list(models.keys()).index(name) + 1} Training {name}...')
+
+            # Fit model on balanced training data
+            model.fit(self.X_train_balanced, self.y_train_balanced)
+            self.models[name] = model
+
+            # cross validation scores
+            from sklearn.model_selection import cross_val_score
+            cv_score = cross_val_score(model, self.X_train_balanced, self.y_train_balanced, cv = cv, scoring= 'accuracy')
+
+            print(f'5-fold CV Accuracy: {cv_score.mean():.4f} (+/- {cv_score.std() * 2:.4f})')
+
+            # Prediction on test set
+            y_pred = model.predict(self.X_test)
+            # Classification report
+            print(f'\n Classification report for {name}: ')
+            from sklearn.metrics import classification_report
+            print(classification_report(self.y_test, y_pred))
+
+            # Store model performance
+            self.model_score[name] = {
+                'cv_mean': cv_score.mean(),
+                'cv_std': cv_score.std(),
+                'predictions': y_pred
+            }
+
+                
+            
+
 
 
     def run_complete_analysis(self):
@@ -153,7 +328,15 @@ class HRAnalyzer:
         self.exploratory_data_analytics()
 
         # Step 3: Clustering
-        # self.employee_clustering()
+        self.employee_clustering()
+
+        # Step 4: Handle class imbalance
+        self.handle_class_imbalance()
+
+        # Step 5: Model training
+        self.train_and_evaluate_models()
+
+        # Step 6: Best Model identification:
 
 
 
