ba5e37ee12c4d12922ea3b6bb733ba70c3498dfc
Anurag <anuragawasthi020@gmail.com>
2025-12-02 23:08:43 +0530

    Saperated the features and label cols, Scaled using robust and Minmax scaler

--- Module 4/4.BinaryClassification.py
+++ Module 4/4.BinaryClassification.py
@@ -3,8 +3,42 @@
 
 import pandas as pd
 import numpy as np
-
+from scipy.stats import shapiro
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 
 data = pd.read_csv('Social_Network_Ads.csv')
 print(data.head(5))
-print(data.info())
+
+# 1. Check the data quality and perform Preprocessing:
+print(data.isna().sum()) # No null values
+# Age                0
+# EstimatedSalary    0
+# Purchased          0
+
+# 2. Splitting data in features and labels:
+features = data.iloc[:,[0,1]].iloc
+labels = data.iloc[:,[2]].iloc
+
+# 3. Check if data is Normally distributed or not:
+cols = ['Age', 'EstimatedSalary', 'Purchased']
+
+def get_scaler(cols):
+    threshold = 0.05
+    for col in cols:
+        print(col)
+        curr, p_val = shapiro(data[col])
+
+        if p_val >= threshold:
+            print(f'{col} Data is normally distributed')
+            return StandardScaler()
+        else:
+            print(f'{col} Data is not normally distributed')
+            return RobustScaler()
+
+rs = RobustScaler()
+s_age = rs.fit_transform(features[:, [0,1]])
+
+mn = MinMaxScaler()
+s_purchased = mn.fit_transform(labels[:, [2]])
+
+# 
